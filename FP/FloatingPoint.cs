bd7b0b50a6bdb4be0f3ddaa85404f05d369d4e86
fernal73 <linus.fernandes@gmail.com>
2020-05-30 15:09:19 +0530

    Add more code to example

--- FP/FloatingPoint.cs
+++ FP/FloatingPoint.cs
@@ -15,6 +15,31 @@ namespace Intro {
       double infinity = Double.PositiveInfinity;
       // Infinity
       Console.WriteLine(infinity);
+
+      // Declare some variables
+      floatPI = 3.141592653589793238f;
+      doublePI = 3.141592653589793238;
+      // Print the results on the console
+      Console.WriteLine("Float PI is: " + floatPI);
+      Console.WriteLine("Double PI is: " + doublePI);
+      // Console output:
+      // Float PI is: 3.141593
+      // Double PI is: 3.14159265358979
+      float f = 0.1f;
+      Console.WriteLine(f);
+      // 0.1 (correct due to rounding)
+      double d = 0.1f;
+      Console.WriteLine(d);
+      // 0.100000001490116 (incorrect)
+      float ff = 1.0f / 3;
+      Console.WriteLine(ff);
+      // 0.3333333 (correct due to rounding)
+      double dd = ff;
+      Console.WriteLine(dd);
+      // 0.333333343267441 (incorrect)
+      decimal decimalPI = 3.14159265358979323846m;
+      Console.WriteLine(decimalPI);
+      // 3.14159265358979323846
     }
   }
 }
