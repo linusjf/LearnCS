19490f6cbbc1432604eb43305ada7d17e965f4fd
fernal73 <linus.fernandes@gmail.com>
2020-05-31 18:19:34 +0530

    Add bitwise operators

--- Operators/Operators.cs
+++ Operators/Operators.cs
@@ -5,6 +5,7 @@ namespace Intro {
     public static void Main(string[] args) {
       MathOperators();
       LogicalOperators();
+      BitWiseOperators();
     }
 
     public static void MathOperators() {
@@ -56,6 +57,27 @@ namespace Intro {
       Console.WriteLine(b || true);
       // False
       Console.WriteLine((5 > 7) ^ (a == b));
+    }
+
+    public static void BitWiseOperators() {
+      // 0000 0011 = 3
+      byte a = 3;
+      // 0000 0101 = 5
+      byte b = 5;
+      // 0000 0111 = 7
+      Console.WriteLine(a | b);
+      // 0000 0001 = 1
+      Console.WriteLine(a & b);
+      // 0000 0110 = 6
+      Console.WriteLine(a ^ b);
+      // 0000 0100 = 4
+      Console.WriteLine(~a & b);
+      // 0000 0110 = 6
+      Console.WriteLine(a << 1);
+      // 0000 1100 = 12
+      Console.WriteLine(a << 2);
+      // 0000 0001 = 1
+      Console.WriteLine(a >> 1);
     }
   }
 }
